9a691989b4e4c8055a47f6a561a5d86da50322eb
Natalie <natdavis@mills.edu>
2021-11-03 19:38:24 -0700

    Squashed commit of the following:
    commit 76a183c803557a516b64f2e5e859f4bf58bb0bbd
    Author: Natalie <natdavis@mills.edu>
    Date:   Wed Nov 3 19:38:07 2021 -0700
    
        mob next [ci-skip] [ci skip] [skip ci]
    
    commit a4aee5c856d6db4f7a58fa42df43fa4034ecd0d6
    Author: hannela <hjane@mills.edu>
    Date:   Wed Nov 3 19:30:26 2021 -0700
    
        mob next [ci-skip] [ci skip] [skip ci]
    
    Co-authored-by: hannela <hjane@mills.edu>

---
+++ src/Frame.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class Frame extends JFrame {
+
+    public Frame() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setPreferredSize(new Dimension(1060, 740));
+        //add
+        //this.pack();
+        this.setLocationRelativeTo(null);
+        //initComponents();
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,12 @@ import java.util.*;
 import javax.swing.*;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("test");
+    public static void main(String[] args){
+        try {
+            Frame test = new Frame();
+        } catch (IOException e){
+            System.out.println("could not start app" + e.getMessage());
+        }
     }
 
 
